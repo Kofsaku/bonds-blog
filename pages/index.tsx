7cea7395503922cb77136f8807c20a7b3ea6ba4a
Kofsaku <51185002+Kofsaku@users.noreply.github.com>
2021-02-23 17:39:50 +0900

    エラーを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,52 @@
 import Head from 'next/head'
-import { getPreEmitDiagnostics } from 'typescript'
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
 
-const {BLOG_URL,CONTENT_API_KEY} = process.env
+const { BLOG_URL, CONTENT_API_KEY } = process.env
 
 type Post = {
-  title: string
-  slug: string
+	title: string
+	slug: string
 }
 
-async function getPosts(){
-  //curl "https://demo.ghost.io/ghost/api/v3/content/posts/?key=22444f78447824223cefc48062"
-  const res = await fetch(`
-  ${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,reading_time`)
-  .then((res) => res.json())
-  //ghostからこれをとってくる&fields=title,url`).　欲しいものを追加 slugとか
+async function getPosts() {
+	// curl ""
+	const res = await fetch(
+		`${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt`
+	).then((res) => res.json())
 
-  const posts = res.posts
+	const posts = res.posts
 
-  return posts
+	return posts
 }
+
 export const getStaticProps = async ({ params }) => {
-  const posts = await getPosts ()
-  return {
-    props: { posts }
-  }
+	const posts = await getPosts()
+	return {
+		revalidate: 10,
+		props: { posts }
+	}
 }
 
-const Home: React.FC<{props: Post[] }> =(props) => {
-  const { posts } = props
-  return (
-  <div className={styles.container}>
-    <h1>Hello to my blog</h1>
-    <ul>
-      {posts.map((post, index) => {
-        return (
-        <li key={post.slug}>
-          <Link href="/post/[slug]" as={`/post/${post.slug}`}>
-              <a>{post.title}</a>
-          </Link>
-        </li>
-        )
-      })}
-    </ul>
-  </div>
-  )
+const Home: React.FC<{ posts: Post[] }> = (props) => {
+	const { posts } = props
+
+	return (
+		<div className={styles.container}>
+			<h1>Hello to my blog</h1>
+			<ul>
+				{posts.map((post, index) => {
+					return (
+						<li className={styles.postitem} key={post.slug}>
+							<Link href="/post/[slug]" as={`/post/${post.slug}`}>
+								<a>{post.title}</a>
+							</Link>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
 }
 
 export default Home
-
-
-//const titles = res.map((post) => post.title)これではだめなぜならpostsはキーだから
-//console.log(titles) 
-//ここで終わるとエラーが出る。returnしないとだめ
-//それぞれの記事を表示させるためにmap
-//return titles
-//ここまでやるとcosoleでjsonを確認できる
